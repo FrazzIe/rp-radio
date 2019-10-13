9ed565b931cbd874401fdb1e73077107b2c7037b
Fraser <28698343+FrazzIe@users.noreply.github.com>
2019-10-13 17:32:56 +0100

    Add __resource info

--- rp-radio/__resource.lua
+++ rp-radio/__resource.lua
@@ -1,5 +1,8 @@
 resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
-
+name "rp-radio"
+description "An in-game radio which makes use of the TokoVOIP radio API for FiveM"
+author "Frazzle (frazzle9999@gmail.com)"
+version "v1.0"
 ui_page "index.html"
 
 dependencies {
